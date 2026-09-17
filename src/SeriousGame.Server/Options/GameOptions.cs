b77d9a63f7c5e562e23bc3e7c609203ce675d00f
erwan749 <101586302+erwan749@users.noreply.github.com>
2026-09-17 10:17:23 +0200

    Revert "add CompanyDto + add CompanyFactory + initial Treasury in Company + setup JSON options"

--- src/SeriousGame.Server/Options/GameOptions.cs
+++ src/SeriousGame.Server/Options/GameOptions.cs
@@ -9,5 +9,4 @@ public class GameOptions
     public int MinimumPlayers { get; init; } = 3;
     public int MaximumPlayers { get; init; } = 8;
     public int RoundsNumber { get; init; } = 15;
-    public int CompanyInitialTreasury { get; init; } = 1000000;
 }
--- src/SeriousGame.Shared/Models/Dtos/CompanyDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared.Models.Dtos;
-
-public class CompanyDto
-{
-    public required string Id { get; init; }
-    public required string Name { get; init; }
-    public required string OwnerId { get; init; }
-    public int Treasury { get; init; }
-}
